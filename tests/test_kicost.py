440141f4cedeb237e4cc1b186c2a53ecb2f5e01b
Salvador E. Tropea <salvador@inti.gob.ar>
2021-03-10 18:12:47 -0300

    Fixed missing results dir

--- tests/test_kicost.py
+++ tests/test_kicost.py
@@ -20,9 +20,13 @@ from kicost import kicost
 def do_test_single(pattern):
     os.chdir('tests')
     fail = False
+    if not os.path.isdir('result_test'):
+        os.mkdir('result_test')
     try:
         for f in glob.glob(pattern):
             try:
+                cmd = ['./test_single.sh', '--no_price', f]
+                logging.debug('Running '+str(cmd))
                 res = subprocess.check_output(['./test_single.sh', '--no_price', f])
                 logging.info(f + ' OK')
             except subprocess.CalledProcessError:
