0feab47006e0b88d5dae8d1f51087cf33c874ee6
Salvador E. Tropea <salvador@inti.gob.ar>
2021-03-10 17:38:43 -0300

    Integrated the tests/test.sh script with PyTest

--- tests/test_kicost.py
+++ tests/test_kicost.py
@@ -9,8 +9,37 @@ Tests for `kicost` module.
 """
 
 import unittest
+import subprocess
+import logging
+import glob
+import os
 
 from kicost import kicost
+
+
+def do_test_single(pattern):
+    os.chdir('tests')
+    fail = False
+    try:
+        for f in glob.glob(pattern):
+            try:
+                res = subprocess.check_output(['./test_single.sh', '--no_price', f])
+                logging.info(f + ' OK')
+            except subprocess.CalledProcessError:
+                logging.error('Failed test: '+f)
+                fail = True
+                pass
+    finally:
+        os.chdir('..')
+    return fail
+
+
+def test_xmls():
+    assert not do_test_single('*.xml')
+
+
+def test_csvs():
+    assert not do_test_single('*.csv')
 
 
 class TestKicost(unittest.TestCase):
